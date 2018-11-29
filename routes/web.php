4d6fbac2c70fb41948ac7b8d872abadda1fa2d70
Desktop(Games) <ka33145@googlemail.com>
2018-11-29 03:06:05 +0000

    Added dummy routes and views

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,13 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('root');
+// Route::get('/', function () {
+//     return view('welcome');
+// })->name('root');
 
+Route::get('/', function () {
+    return view('welcome2');
+})->name('root');
 
 Route::resource('/restaurants', 'RestaurantController');
 
@@ -30,3 +33,59 @@ Route::get('/home', 'HomeController@index')->name('home');
 // function () {return view('welcome');},
 // 'as'=>'root'
 // );
+
+
+//About links
+Route::get('/about',function(){
+    $myTitle = 'About';
+    return view('temp', compact('myTitle'));
+})->name('about');
+
+Route::get('/about/history',function(){
+    $myTitle = 'Company History';
+    return view('temp', compact('myTitle'));
+})->name('about.history');
+
+Route::get('/about/team',function(){
+    $myTitle = 'The Team';
+    return view('temp', compact('myTitle'));
+})->name('about.team');
+
+
+//Support links
+Route::get('/support',function(){
+     $myTitle = 'Support';
+     return view('temp', compact('myTitle'));
+})->name('support');
+
+Route::get('/support/contact',function(){
+     $myTitle = 'Contact';
+     return view('temp', compact('myTitle'));
+})->name('support.contact');
+
+Route::get('/support/faqs',function(){
+     $myTitle = 'FAQs';
+     return view('temp', compact('myTitle'));
+})->name('support.faqs');
+
+
+//Legal links
+Route::get('/legal',function(){
+    $myTitle = 'Legal';
+    return view('temp', compact('myTitle'));
+})->name('legal');
+
+Route::get('/legal/terms',function(){
+     $myTitle = 'Terms & Conditions';
+     return view('temp', compact('myTitle'));
+})->name('legal.terms');
+
+Route::get('/legal/privacy',function(){
+     $myTitle = 'Privacy Policy';
+     return view('temp', compact('myTitle'));
+})->name('legal.privacy');
+
+Route::get('/legal/cookie',function(){
+     $myTitle = 'Cookie Policy';
+     return view('temp', compact('myTitle'));
+})->name('legal.cookie');
